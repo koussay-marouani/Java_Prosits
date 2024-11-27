14d96d3ce47542005d5f16b02bddea0fd8da0291
koussay-marouani <koussay.marouani@esprit.tn>
2024-11-27 21:53:06 +0100

    prosit1

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,26 +1,14 @@
 import java.util.Scanner;
 
 public class ZooManagement {
+
+
     public static void main(String[] args) {
-        int nbrCages = 20;
-        String zooName = "my zoo";
-
-        System.out.println(zooName + " comporte " + nbrCages + " cages");
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Entrez le nom du zoo : ");
-        zooName = scanner.nextLine();
-
-        System.out.print("Entrez le nombre de cages : ");
-        while (!scanner.hasNextInt()) {
-            System.out.println("Veuillez entrer un nombre valide pour le nombre de cages.");
-            scanner.next();
-        }
-        nbrCages = scanner.nextInt();
-
-        System.out.println(zooName + " comporte " + nbrCages + " cages");
-
-        scanner.close();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Donnez le nom du zoo : ");
+        String zooName = sc.next();
+        System.out.println("Donnez le nombre des cages : ");
+        int nbrCages = sc.nextInt();
+        System.out.println(zooName + " comporte " + nbrCages + " cages.");
     }
 }
